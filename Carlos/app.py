cb7da2760985dfa5d82516a71454ee6fc8527f10
Carlos Yalta Vargas <67847096+carlosyv@users.noreply.github.com>
2021-04-22 00:50:50 -0500

    create list + dropdown

--- Carlos/app.py
+++ Carlos/app.py
@@ -23,11 +23,17 @@ df = pd.read_csv('data/stockdata2.csv', parse_dates=['Date'], usecols=['Date',
 # Initialise the app
 app = dash.Dash(__name__)
 
+
+# Creates a list of dictionaries, which have the keys 'label' and 'value'
+
+def get_options(list_stocks):
+	dict_list = []
+	for i in list_stocks:
+		dict_list.append({'label': i,'value': i})
+
+	return dict_list
+
 # Define the app
-app.layout = html.Div()
-
-
-
 app.layout = html.Div(children=[
 					html.Div(className="row",
 							children=[
@@ -37,7 +43,17 @@ app.layout = html.Div(children=[
 										html.P("""Visualising time series with 
 													Plotly - Dash"""),
 										html.P("""Pick one or more stocks 
-												from the dropdown below.""")
+												from the dropdown below."""),
+										html.Div(className='div-for-dropdown',
+											children=[
+												dcc.Dropdown(id='stockselector',
+															options=get_options(df['stock'].unique()),
+															multi=True,
+															value=[df['stock'].sort_values()[0]],
+															style={'backgroundColor': '#1E1E1E'},
+															className='stockselector'
+													)],
+												style={'color': '#1E1E1E'})
 											]),
 # Defining the left element
 								html.Div(className="eight columns div-for-charts bg-grey",
